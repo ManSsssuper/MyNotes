430121dd8e4c1c86cad69063e564925d39662ec4
ManSsssuper <1335735422@qq.com>
2019-11-14 22:35:55 +0800

    new version

--- Code/PyTorch/FirstNN.py
+++ Code/PyTorch/FirstNN.py
@@ -13,9 +13,10 @@ class Net(nn.Module):
         super(Net,self).__init__()
         self.conv1=nn.Conv2d(1,6,5)
         self.conv2=nn.Conv2d(6,16,5)
-        self.fc1=nn.Linear(16*5*5,120)
         self.fc2=nn.Linear(120,84)
         self.fc3=nn.Linear(84,10)
+        self.fc1=nn.Linear(16*5*5,120)
+
     def forward(self,x):
         x=F.max_pool2d(F.relu(self.conv1(x)),(2,2))
         x=F.max_pool2d(F.relu(self.conv2(x)),2)
